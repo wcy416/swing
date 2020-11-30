02d986aa25104746f30a8f6b0c08239b7405c8c2
wcy416 <74659347+wcy416@users.noreply.github.com>
2020-11-30 11:53:49 +0800

    接口

---
+++ 113002.接口.R.java
@@ -0,0 +1,10 @@
+package pac;
+
+public interface R {
+	static void aa() {
+		String boy_filePath="img/boy.gif";
+		String girl_filePath="img/girl.gif";
+		int [][]array2d= {{1,2,3},{4,5,6}};
+		System.out.println(boy_filePath+"\n"+girl_filePath+"\n"+array2d[0][1]);
+	}
+}
