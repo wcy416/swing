29ee306383f5a43ebf8b0828d73313968fbf7b58
wcy416 <74659347+wcy416@users.noreply.github.com>
2020-12-08 14:20:47 +0800

    0相邻的主类

---
+++ 120701.主类.App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3, 4, 5, 6},
+                {7, 8, 9, 10, 0, 12},
+                {8, 8, 9, 10, 11, 12},
+                {7, 8, 3, 10, 11, 12},
+                {88, 66, 92, 102, 151, 999}
+        };
+        new Find().arrayFindValue(array);
+    }
+}
